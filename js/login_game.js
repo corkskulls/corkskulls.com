d9771d07c0b0f7357cf02d1f38788b2efda1a544
Summit-Head <129321766+Summit-Head@users.noreply.github.com>
2025-01-31 18:33:13 -0800

    password
    added password game and js folder

---
+++ js/login_game.js
@@ -0,0 +1,12 @@
+
+const form = document.getElementById('loginForm');
+const forgotForm = document.getElementById('forgotForm');
+const findUse = document.getElementById('findUse');
+const findEmail = document.getElementById('findEmail');
+const changePass = document.getElementById('passChange');
+const changeUse = document.getElementById('useChange');
+const findID = document.getElementById('employeeCreds');
+form.addEventListener('submit', (event) => {
+// Add first login form stuff here 
+
+});
